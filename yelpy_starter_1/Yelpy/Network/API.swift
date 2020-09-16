5e7c7ae8bbe97834b3bc971f9a69c9feac504541
Nathan Latronica <nathanlatronica@Latronica-MacBook-Pro.local>
2020-09-15 17:20:05 -0700

    lab is done

---
+++ yelpy_starter_1/Yelpy/Network/API.swift
@@ -0,0 +1,63 @@
+//
+//  File.swift
+//  Yelpy
+//
+//  Created by Memo on 5/21/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import Foundation
+
+
+struct API {
+    
+
+    
+    static func getRestaurants(completion: @escaping ([[String:Any]]?) -> Void) {
+        
+        // ––––– TODO: Add your own API key!
+        let apikey = "BVJe9a9mcWyXJwl4fZcB_5JAOigCiDqEAuN3Q8OqK8JffshNNwUsU8r-ksXoWgJg9qVEodvxJOusOWxX6J58_vbeom0WPr9jR-cKmTXgk7_4N3Fhc_g_fP64N09hX3Yx"
+        
+        // Coordinates for San Francisco
+        let lat = 37.773972
+        let long = -122.431297
+        
+        
+        let url = URL(string: "https://api.yelp.com/v3/transactions/delivery/search?latitude=\(lat)&longitude=\(long)")!
+        
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        // Insert API Key to request
+        request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                
+        
+
+                // ––––– TODO: Get data from API and return it using completion
+                print(data)
+                
+                //1.
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                //2.
+                let restaurants = dataDictionary["businesses"] as! [[String: Any]]
+                //3.
+                return completion(restaurants)
+                
+                
+                return completion([[:]])
+                
+                }
+            }
+        
+            task.resume()
+        
+        }
+    }
+
+    
